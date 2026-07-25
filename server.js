cc2a1b88910d42a5d5566bd118fec8fb0040dc24
Your Name <your.email@example.com>
2026-07-25 13:48:06 +0200

    Use exact environment port without static fallback

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
